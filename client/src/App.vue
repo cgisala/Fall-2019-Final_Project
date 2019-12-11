9ad167965741b93d3ba85b3a9793f70bf338d7c5
chris <chris@chris.com>
2019-12-10 19:20:43 -0600

    Stopped at Slide 43

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header></Header>
+    <!-- todo - content -->
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header, Footer
   }
 }
 </script>
